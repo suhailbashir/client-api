f87af3ce634f9efdad5f0b70cf2080c4a9c8064b
suhail <suhailbashirbhat@gmail.com>
2021-04-14 16:49:17 +0530

    test changes

---
+++ src/test/java/com/sapient/client/ClientResourceTest.java
@@ -0,0 +1,86 @@
+package com.sapient.client;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+import com.sapient.client.beans.AddressBean;
+import com.sapient.client.beans.ClientBean;
+import com.sapient.client.beans.EmiBean;
+import com.sapient.client.beans.LoanBean;
+import com.sapient.client.controller.ClientResource;
+import com.sapient.client.service.ClientService;
+
+
+public class ClientResourceTest {
+
+	@Mock
+	ClientService clientService;
+	
+	@InjectMocks
+	ClientResource clientResource;
+	
+	
+
+	@Test
+	@Order(1)
+	@DisplayName("TESTING SAVE METHOD")
+	public void saveClient() {
+		
+		ClientBean clientBean=createClientBean();
+		when(clientResource.saveClient(clientBean)).thenReturn(ResponseEntity.status(HttpStatus.CREATED).body(clientBean));
+		assertEquals(clientBean, clientService.saveClient(clientBean));
+	}
+
+	private ClientBean createClientBean() {
+		
+		EmiBean emiBean=EmiBean.builder()
+								.id(5L)
+								.number(1L).amount(100.0)
+								.dueDate(LocalDate.now())
+								.build();
+		
+		List<EmiBean>emiBeans=Arrays.asList(emiBean);
+		
+		LoanBean loanBean=LoanBean.builder()
+								  .id(4L).loanAccountNumber("123_LAN_222")
+								  .loanType("Saving")
+								  .listOfEmis(emiBeans)
+								  .build();
+		
+		List<LoanBean>loanBeans=Arrays.asList(loanBean);
+		
+		AddressBean a1=	AddressBean.builder()
+								   .id(2L)
+								   .street("124")
+								   .State("Rajasthan")
+								   .Country("India")
+								   .zip("67676")
+								   .build();
+		
+		AddressBean a2=	AddressBean.builder()
+								   .id(3L)
+								   .city("Srinagar")
+								   .street("12")
+								   .State("JK")
+								   .Country("India")
+								   .zip("8888")
+								   .build();
+		
+		List<AddressBean>addressBeans=Arrays.asList(a1,a2);
+		
+		return ClientBean.builder().id(1L).clientName("Suhail").loans(loanBeans).addresses(addressBeans).build();
+	}
+	
+}
